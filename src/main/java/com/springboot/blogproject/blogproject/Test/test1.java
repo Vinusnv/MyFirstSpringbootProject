debbd247d0b0f143209f05de4b16aa7f3826ca4d
Vinay <vsnv9964@gmail.com>
2022-08-19 00:22:36 +0530

    added test folder and created new test1 file in it

---
+++ src/main/java/com/springboot/blogproject/blogproject/Test/test1.java
@@ -0,0 +1,11 @@
+package com.springboot.blogproject.blogproject.Test;
+
+public class test1 {
+
+    void meth()
+    {
+        System.out.println("welcome to git");
+       // vfddg
+    }
+    
+}
